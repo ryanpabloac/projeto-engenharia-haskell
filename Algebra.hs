8bc318971073539795f64b06a35af447f2a84c4d
diogoPontes248 <didi248pvc.d.p.v.c@gmail.com>
2025-09-04 08:56:29 -0300

    Update Algebra.hs

--- Algebra.hs
+++ Algebra.hs
@@ -1,2 +1,37 @@
 -- 4 funções iniciais => Cadu 
--- 4 finais => Diogo
+-- falta a função resolverSistemasLineares => Diogo
+module LinearAlgebra where
+
+import Types (Angulo, Matriz(..), Vetor(..))
+
+
+
+
+
+
+
+
+
+
+
+
+produtoEscalar :: Vetor -> Vetor -> Maybe Double
+produtoEscalar _ (Vetor []) = Nothing
+produtoEscalar (Vetor xs) (Vetor ys)
+    |(length xs) == (length ys) = Just $ sum $ zipWith (*) xs ys
+    |otherwise = Nothing
+
+normaVetor :: Vetor -> Double
+normaVetor (Vetor []) = 0
+normaVetor (Vetor xs) = sqrt $ sum $ map (**2) xs
+
+anguloEntreVetores :: Vetor -> Vetor -> Maybe Angulo
+anguloEntreVetores (Vetor xs) (Vetor ys)
+    | xs == [] = Nothing
+    | length xs /= length ys = Nothing
+    | nx == 0 || ny == 0 = Nothing
+    | otherwise = Just $ acos $ pe / ((nx) * (ny))
+  where
+    pe = sum $ zipWith (*) xs ys
+    nx = normaVetor (Vetor xs)
+    ny = normaVetor (Vetor ys)
