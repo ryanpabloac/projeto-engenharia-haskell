8727a18b7b4223664bc7de6847bef97aa1352e92
Mithgmmt14 <matheusuec@gmail.com>
2025-09-04 09:58:47 -0300

    Update Civil.hs

--- Engineering/Civil.hs
+++ Engineering/Civil.hs
@@ -7,6 +7,19 @@ momentoInerciaRetangular largura_base altura_seccao =
    (largura_base * (altura_seccao ^ 3)) / 12
    
 tensaoNormal :: Forca -> Area -> Pressao
-tensaoNormal _ 0 = error "Área de seção não pode ser 0 (divisão por 0 - indeterminação)"
+tensaoNormal _ 0 = error  error "Erro: Área da secção não pode ser zero, acarretando em divisão por zero, gerando indeterminação"
 tensaoNormal forca_aplicada a_seccao_transversal =
    forca_aplicada / a_seccao_transversal
+
+deflexaoViga :: Forca-> Comprimento-> ModuloElasticidade-> MomentoInercia-> Distancia
+deflexaoViga _ _ 0 _ = error "Erro: Módulo de elasticidade não pode ser zero, acarretando em divisão por zero, gerando indeterminação"
+deflexaoViga _ _ _ 0 = error "Erro: Momento de inércia não pode ser zero, acarretando em divisão por zero, gerando indeterminação"
+deflexaoViga carga comprimento elasticidade inercia = 
+        (carga * comprimento ** 3 ) / ( 48 * elasticidade * inercia )
+
+cargaCriticaEuler :: ModuloElasticidade-> MomentoInercia-> Comprimento-> Forca
+cargaCriticaEuler _ _ 0 = error "Erro: Comprimento não pode ser zero, acarretando em divisão por zero, gerando indeterminação"
+cargaCriticaEuler elasticidade inercia comprimento = 
+        ( pi ** 2 * elasticidade * inercia ) / ( comprimento ** 2 )
+
+
