92d0ed10ab13ca4a85e8eb35b9b35d1eec56e810
Mithgmmt14 <matheusuec@gmail.com>
2025-09-04 10:00:24 -0300

    Create Mechanical.hs

---
+++ Engineering/Mechanical.hs
@@ -0,0 +1,22 @@
+module 
+
+calcularTorque :: Forca-> Distancia-> Angulo-> Torque
+calcularTorque força distancia_eixo angulo_força_raio = 
+        força * distancia_eixo * sin angulo_força_raio 
+
+velocidadeAngular :: Velocidade-> Raio-> VelocidadeAngular
+velocidadeAngular _ 0 = error "Erro: Módulo do raio não pode ser zero, acarretando em divisão por zero, gerando indeterminação"
+velocidadeAngular velocidade_linear raio = 
+        velocidade_linear / raio
+
+aceleracaocentripeta :: Velocidade-> Raio-> Aceleracao
+aceleracaocentripeta _ 0 = error "Erro: Módulo do raio não pode ser zero, acarretando em divisão por zero, gerando indeterminação"
+aceleracaocentripeta velocidade_tangecial raio = 
+        (velocidade_tangecial ** 2) / raio
+        
+energiaCinetica :: Massa-> Velocidade-> Energia
+energiaCinetica massa velocidade = 0.5 * massa * velocidade ** 2
+
+energiaPotencial :: Massa-> Altura-> Energia
+energiaPotencial massa altura = massa * 9.81 * altura 
+
