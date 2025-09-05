0f966140671e2f5d8a50c26ce5832ecf0b45c74a
Mithgmmt14 <matheusuec@gmail.com>
2025-09-05 13:41:06 -0300

    Update Calculus.hs

--- Calculus.hs
+++ Calculus.hs
@@ -4,9 +4,10 @@ import Types
 
 integralNumerica :: Funcao -> Double -> Double -> Int -> Double -- Cálculo de Integral com Base na Soma de Riemann;
 integralNumerica funcao limite_inferior limite_superior n = -- limites de integração e a quantidade de retângulos para representar a função;
-    sum [ avaliarFuncao funcao ( limite_inferior + base * fromIntegral posicao ) * base | posicao <- [0 .. n - 1]] -- Realiza a Soma das àrea dos retângulos, área calculada por f (x) * base;
-        where 
-            base = ( limite_superior - limite_inferior ) / fromIntegral n -- base de cada retângulo;
+  if limite_inferior > limite_superior then error "Erro: limite inferior maior do que limite superior, não é possível calcular a integral" else
+        sum [ avaliarFuncao funcao ( limite_inferior + base * fromIntegral posicao ) * base | posicao <- [0 .. n - 1]] -- Realiza a Soma das àrea dos retângulos, área calculada por f (x) * base;
+            where 
+                base = ( limite_superior - limite_inferior ) / fromIntegral n -- base de cada retângulo;
 
 derivadaNumerica :: Funcao-> Double-> Double -- Cálculo de Derivada pela definição newtoniana
 derivadaNumerica funcao x = 
