cf737e8c5f67cdf4ef6fbd6ab0fd1a31319ab192
Mithgmmt14 <matheusuec@gmail.com>
2025-09-05 13:43:12 -0300

    Update Calculus.hs

--- Calculus.hs
+++ Calculus.hs
@@ -4,13 +4,11 @@ import Types
 
 integralNumerica :: Funcao -> Double -> Double -> Int -> Double -- Cálculo de Integral com Base na Soma de Riemann;
 integralNumerica funcao limite_inferior limite_superior n = -- limites de integração e a quantidade de retângulos para representar a função;
-  if limite_inferior > limite_superior then error "Erro: limite inferior maior do que limite superior, não é possível calcular a integral" else
-        sum [ avaliarFuncao funcao ( limite_inferior + base * fromIntegral posicao ) * base | posicao <- [0 .. n - 1]] -- Realiza a Soma das àrea dos retângulos, área calculada por f (x) * base;
-            where 
-                base = ( limite_superior - limite_inferior ) / fromIntegral n -- base de cada retângulo;
+   | limite_inferior > limite_superior = error "Erro: limite inferior maior do que limite superior, não é possível calcular a integral" else
+   | otherwise = sum [ avaliarFuncao funcao ( limite_inferior + base * fromIntegral posicao ) * base | posicao <- [0 .. n - 1]] -- Realiza a Soma das àrea dos retângulos, área calculada por f (x) * base;
+            where base = ( limite_superior - limite_inferior ) / fromIntegral n -- base de cada retângulo;
 
 derivadaNumerica :: Funcao-> Double-> Double -- Cálculo de Derivada pela definição newtoniana
 derivadaNumerica funcao x = 
-        ( avaliarFuncao funcao ( x + dx ) - avaliarFuncao funcao ( x )) / dx -- f' (x) ~= (f ( x + dx ) + f (x)) / dx
-        where 
-            dx = 1e-9
+    ( avaliarFuncao funcao ( x + dx ) - avaliarFuncao funcao ( x )) / dx -- f' (x) ~= (f ( x + dx ) + f (x)) / dx
+        where dx = 1e-9
