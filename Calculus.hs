f1844f08e27c9485dd7e5abd3166a28e22405b74
Mithgmmt14 <matheusuec@gmail.com>
2025-09-05 13:16:54 -0300

    Update Calculus.hs

--- Calculus.hs
+++ Calculus.hs
@@ -1,10 +1,10 @@
+module Calculus where
 
-
+import Types
 
 integralNumerica :: Funcao -> Double -> Double -> Int -> Double -- Cálculo de Integral com Base na Soma de Riemann;
 integralNumerica funcao limite_inferior limite_superior n = -- limites de integração e a quantidade de retângulos para representar a função;
-    sum [ avaliarFuncao funcao ( limite_inferior + base * fromIntegral posicao ) * base | posicao <- [0 .. n - 1]]
-        -- Realiza a Soma das àrea dos retângulos, área calculada por f (x) * base;
+    sum [ avaliarFuncao funcao ( limite_inferior + base * fromIntegral posicao ) * base | posicao <- [0 .. n - 1]] -- Realiza a Soma das àrea dos retângulos, área calculada por f (x) * base;
         where 
             base = ( limite_superior - limite_inferior ) / fromIntegral n -- base de cada retângulo;
 
