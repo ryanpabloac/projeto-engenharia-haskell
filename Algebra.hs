863235755b2fb2c17dd4e997dc25ecb523cf5b00
diogoPontes248 <didi248pvc.d.p.v.c@gmail.com>
2025-09-07 13:34:03 +0000

    Update Algebra.hs

--- Algebra.hs
+++ Algebra.hs
@@ -32,14 +32,14 @@ resolverSistemaLinear (Matriz xss) (Vetor ys)
 produtoEscalar :: Vetor -> Vetor -> Maybe Double
 produtoEscalar _ (Vetor []) = Nothing
 produtoEscalar (Vetor xs) (Vetor ys)
-    |(length xs) == (length ys) = Just $ sum $ zipWith (*) xs ys
+    |(length xs) == (length ys) = Just $ sum $ zipWith (*) xs ys -- o zipWith multiplica cada elemento de um vetor no elemento correnpondente do outro vetor, depois faz a soma.
     |otherwise = Nothing
 
 normaVetor :: Vetor -> Double
 normaVetor (Vetor []) = 0
-normaVetor (Vetor xs) = sqrt $ sum $ map (**2) xs
+normaVetor (Vetor xs) = sqrt $ sum $ map (**2) xs --o map eleva todos os elementos ao quadrado, depois soma todos os elementos e tira a raiz. 
 
-anguloEntreVetores :: Vetor -> Vetor -> Maybe Angulo
+anguloEntreVetores :: Vetor -> Vetor -> Maybe Angulo -- usando a formula angulo = acos do produto escalar dividido pela multiplicacao das normas dos vetores
 anguloEntreVetores (Vetor xs) (Vetor ys)
     | xs == [] = Nothing
     | length xs /= length ys = Nothing
