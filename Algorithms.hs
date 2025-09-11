2bac629c7b7f4d969c515fa92807c7fb046561fa
Caio Fernandes <caiofernandes250505@gmail.com>
2025-09-11 10:52:21 -0300

    Algoritmos  e estrutura de dados está pronto (6)

--- Algorithms.hs
+++ Algorithms.hs
@@ -25,3 +25,47 @@ quickSort (x:xs) = quickSort menor ++ [x] ++ quickSort maior
     where
         menor = [y | y <- xs, y < x]
         maior = [y | y <- xs, y >= x]
+
+-- função insertion sort (destruir e reconstruir)
+
+insertionSort :: (Ord a) => [a] -> [a]
+insertionSort [] = []
+insertionSort (x:xs) = inserir x (insertionSort xs)
+  where
+    inserir y [] = [y]
+    inserir y (z:zs)
+      | y <= z = y:z:zs
+      | otherwise = z : inserir y zs
+
+-- função inserir ordenado
+
+inserirOrdenado :: (Ord a) => a -> [a] -> [a]
+inserirOrdenado y [] = [y]
+inserirOrdenado y (z:zs)
+    | y <= z = y:z:zs
+    | otherwise = z : inserirOrdenado y zs
+
+-- estrutura de dados árvore binária
+
+data ArvoreBinaria a = Vazia | No a (ArvoreBinaria a) (ArvoreBinaria a)
+    deriving (Show, Eq)
+
+-- Função construir a árvore a partir da lista (quando usar quarda a arvore)
+construirArvore :: (Ord a) => [a] -> ArvoreBinaria a
+construirArvore = foldr inserirElemento Vazia
+
+-- Função auxiliar para ciar os nos da arvore
+inserirElemento :: (Ord a) => a -> ArvoreBinaria a -> ArvoreBinaria a
+inserirElemento x Vazia = No x Vazia Vazia
+inserirElemento x (No y esq dir)
+    | x < y = No y (inserirElemento x esq) dir
+    | x > y = No y esq (inserirElemento x dir)
+    | otherwise = No y esq dir -- Caso de elemento duplicado, não faz nada
+
+-- Função para buscar um elemento na árvore
+buscarArvore :: (Ord a) => a -> ArvoreBinaria a -> Bool
+buscarArvore _ Vazia = False
+buscarArvore x (No y esq dir)
+    | x == y = True
+    | x < y  = buscarArvore x esq
+    | otherwise = buscarArvore x dir
