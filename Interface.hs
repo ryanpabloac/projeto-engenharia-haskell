af1f996925e6313f93f54ab10a7eb4daa7be430e
Caio Fernandes <caiofernandes250505@gmail.com>
2025-09-11 18:30:05 -0300

    algoritmos e estruturas de dados
    acho q fiz do jeito burro

--- Interface.hs
+++ Interface.hs
@@ -3,192 +3,204 @@ module Interface (hubMenus) where
 import Calculus
 import Geometry
 import Types
+import DataStructures
+import Algorithms 
 
 -- |Funções Gerais
 exibirOpções :: [String] -> IO ()
 exibirOpções opções = do
-   putStrLn ""
-   putStrLn "==== MENU ===="
-   putStrLn "Selecione uma opção válida: "
-   putStrLn $ unlines opções
+    putStrLn ""
+    putStrLn "==== MENU ===="
+    putStrLn "Selecione uma opção válida: "
+    putStrLn $ unlines opções
 
 
 lerOpção :: Int -> Int -> IO Int
 lerOpção min max = do
-   putStr "|-> "
-   opção <- readLn
-   validar opção
-   
-   where
+    putStr "|-> "
+    opção <- readLn
+    validar opção
+    
+    where
       validar op
-            | op >= min && op <= max = return op
-            | otherwise = do 
-                            putStrLn "\nOpção inválida! Selecione outra válida."
-                            lerOpção min max
+          | op >= min && op <= max = return op
+          | otherwise = do 
+                        putStrLn "\nOpção inválida! Selecione outra válida."
+                        lerOpção min max
 
 hubMenus :: String -> IO ()
 hubMenus menu = 
-   case menu of
+    case menu of
       "principal" -> menuPrincipal
       "calculo"   -> menuCálculo
+      "geometria" -> menuGeometria
+      "algoritmos" -> menuAlgoritmos
+      "estruturas_de_dados" -> menuEstruturasDeDados
 
 
 -- |Menus Específicos
 menuPrincipal :: IO ()
 menuPrincipal = do
-   exibirOpções listaOp
-   op <- lerOpção opMin opMax
-   
-   case op of
+    exibirOpções listaOp
+    op <- lerOpção opMin opMax
+    
+    case op of
       0 -> do putStrLn "Programa encerrado!"
               return ()
       3 -> do hubMenus "calculo"
+      4 -> do hubMenus "geometria"
+      5 -> do hubMenus "algoritmos"
+      6 -> do hubMenus "estruturas_de_dados"
       
-   where
+    where
       listaOp = [
-       "[3] Cálculo Diferencial e Integral",
-       "[0] Sair"]
+        "[3] Cálculo Diferencial e Integral",
+        "[4] Geometria",
+        "[5] Algoritmos",
+        "[6] Estruturas de Dados",
+        "[0] Sair"]
       opMin = 0
-      opMax = 6
+      opMax = 6 --mudar
       
- {-
-    "[6] Executar todos os testes",
- -}
+{-
+    "[6] Executar todos os testes", -- trocar a opção
+-}
+
 menuCálculo :: IO ()
 menuCálculo = do
-   exibirOpções listaOp
-   op <- lerOpção opMin opMax
-   f <- menuFunção
-   
-   case op of
+    exibirOpções listaOp
+    op <- lerOpção opMin opMax
+    f <- menuFunção
+    
+    case op of
       0 -> do menuPrincipal
       1 -> do
-         putStr "Entre com um valor x: "
-         x <- readLn
-         let resultado = avaliarFuncao f x
-         putStrLn $ "Resultado: " ++ (show resultado)
-         menuCálculo
-         
+          putStr "Entre com um valor x: "
+          x <- readLn
+          let resultado = avaliarFuncao f x
+          putStrLn $ "Resultado: " ++ (show resultado)
+          menuCálculo
+          
       2 -> do
-         putStr "Entre com um valor x: "
-         x <- readLn
-         let resultado = derivadaNumerica f x
-         putStrLn $ "Resultado: " ++ (show resultado)
-         menuCálculo
-         
+          putStr "Entre com um valor x: "
+          x <- readLn
+          let resultado = derivadaNumerica f x
+          putStrLn $ "Resultado: " ++ (show resultado)
+          menuCálculo
+          
       3 -> do
-         putStr "Limite inferior: "
-         a <- readLn
-         putStr "Limite superior: "
-         b <- readLn
-         putStr "Quantidade de somas: "
-         n <- readLn
-         let resultado = integralNumerica f a b n 
-         putStrLn $ "Resultado: " ++ (show resultado)
-         menuCálculo
-         
+          putStr "Limite inferior: "
+          a <- readLn
+          putStr "Limite superior: "
+          b <- readLn
+          putStr "Quantidade de somas: "
+          n <- readLn
+          let resultado = integralNumerica f a b n 
+          putStrLn $ "Resultado: " ++ (show resultado)
+          menuCálculo
+          
       4 -> do
-         putStr "Limite inferior: "
-         a <- readLn
-         putStr "Limite superior: "
-         b <- readLn
-         let resultado = encontrarRaizes f a b
-         putStrLn $ "Resultado: " ++ (show resultado)
-         menuCálculo
-      
+          putStr "Limite inferior: "
+          a <- readLn
+          putStr "Limite superior: "
+          b <- readLn
+          let resultado = encontrarRaizes f a b
+          putStrLn $ "Resultado: " ++ (show resultado)
+          menuCálculo
+          
       5 -> do
-         putStr "Limite inferior: "
-         a <- readLn
-         putStr "Limite superior: "
-         b <- readLn
-         let resultado = pontosCriticos f a b
-         putStrLn $ "Resultado: " ++ (show resultado)
-         menuCálculo
-         
+          putStr "Limite inferior: "
+          a <- readLn
+          putStr "Limite superior: "
+          b <- readLn
+          let resultado = pontosCriticos f a b
+          putStrLn $ "Resultado: " ++ (show resultado)
+          menuCálculo
+          
       6 -> do
-         putStr "Limite inferior: "
-         a <- readLn
-         putStr "Limite superior: "
-         b <- readLn
-         let resultado = encontrarMinimo f a b
-         putStrLn $ "Resultado: " ++ (show resultado)
-         menuCálculo
-         
+          putStr "Limite inferior: "
+          a <- readLn
+          putStr "Limite superior: "
+          b <- readLn
+          let resultado = encontrarMinimo f a b
+          putStrLn $ "Resultado: " ++ (show resultado)
+          menuCálculo
+          
       7 -> do
-         putStr "Limite inferior: "
-         a <- readLn
-         putStr "Limite superior: "
-         b <- readLn
-         let resultado = encontrarMaximo f a b
-         putStrLn $ "Resultado: " ++ (show resultado)
-         menuCálculo
-           
-   where
+          putStr "Limite inferior: "
+          a <- readLn
+          putStr "Limite superior: "
+          b <- readLn
+          let resultado = encontrarMaximo f a b
+          putStrLn $ "Resultado: " ++ (show resultado)
+          menuCálculo
+            
+    where
       listaOp = [
-       "[1] Avaliar Função",
-       "[2] Derivada",
-       "[3] Integral",
-       "[4] Encontrar Raízes",
-       "[5] Pontos Críticos",
-       "[6] Encontrar Mínimo",
-       "[7] Encontrar Máximo",
-       "[0] Voltar"
-       ]
+        "[1] Avaliar Função",
+        "[2] Derivada",
+        "[3] Integral",
+        "[4] Encontrar Raízes",
+        "[5] Pontos Críticos",
+        "[6] Encontrar Mínimo",
+        "[7] Encontrar Máximo",
+        "[0] Voltar"
+        ]
       opMin = 0
       opMax = 7
  
  
 menuFunção :: IO Funcao
 menuFunção = do
-   exibirOpções listaOp
-   op <- lerOpção opMin opMax
-   
-   case op of
+    exibirOpções listaOp
+    op <- lerOpção opMin opMax
+    
+    case op of
       1 -> do
-         putStr "Entre com o coeficiente a: "
-         a <- readLn
-         putStr "Entre com o coeficiente b: "
-         b <- readLn
-         putStr "Descreva a função aqui: "
-         desc <- getLine
-         return (Funcao (Linear a b) desc) -- ax + b
+          putStr "Entre com o coeficiente a: "
+          a <- readLn
+          putStr "Entre com o coeficiente b: "
+          b <- readLn
+          putStr "Descreva a função aqui: "
+          desc <- getLine
+          return (Funcao (Linear a b) desc) -- ax + b
       2 -> do
-         putStr "Entre com o coeficiente a: "
-         a <- readLn
-         putStr "Entre com o coeficiente b: "
-         b <- readLn
-         putStr "Entre com o coeficiente c: "
-         c <- readLn
-         putStr "Descreva a função aqui: "
-         desc <- getLine
-         return (Funcao (Quadratica a b c) desc) -- ax² + bx + c
+          putStr "Entre com o coeficiente a: "
+          a <- readLn
+          putStr "Entre com o coeficiente b: "
+          b <- readLn
+          putStr "Entre com o coeficiente c: "
+          c <- readLn
+          putStr "Descreva a função aqui: "
+          desc <- getLine
+          return (Funcao (Quadratica a b c) desc) -- ax² + bx + c
       3 -> do
-         putStr "Entre com o coeficiente a: "
-         a <- readLn
-         putStr "Entre com o coeficiente b: "
-         b <- readLn
-         putStr "Descreva a função aqui: "
-         desc <- getLine
-         return (Funcao (Exponencial a b) desc) -- a*e⁽bx⁾
+          putStr "Entre com o coeficiente a: "
+          a <- readLn
+          putStr "Entre com o coeficiente b: "
+          b <- readLn
+          putStr "Descreva a função aqui: "
+          desc <- getLine
+          return (Funcao (Exponencial a b) desc) -- a*e⁽bx⁾
       4 -> do
-         putStr "Entre com o coeficiente a: "
-         a <- readLn
-         putStr "Entre com o coeficiente b: "
-         b <- readLn
-         putStr "Descreva a função aqui: "
-         desc <- getLine
-         return (Funcao (Exponencial a b) desc) -- a*ln(bx)
+          putStr "Entre com o coeficiente a: "
+          a <- readLn
+          putStr "Entre com o coeficiente b: "
+          b <- readLn
+          putStr "Descreva a função aqui: "
+          desc <- getLine
+          return (Funcao (Exponencial a b) desc) -- a*ln(bx)
       5 -> do
-         putStr "Qual função trigonométrica [Seno/Cosseno/Tangente]: "
-         f <- readLn
-         putStr "Entre com o coeficiente a: "
-         a <- readLn
-         putStr "Entre com o coeficiente b: "
-         b <- readLn
-         putStr "Descreva a função aqui: "
-         desc <- getLine
-         return (Funcao (Trigonometrica f a b) desc) -- a*func(bx)      
-   where
+          putStr "Qual função trigonométrica [Seno/Cosseno/Tangente]: "
+          f <- readLn
+          putStr "Entre com o coeficiente a: "
+          a <- readLn
+          putStr "Entre com o coeficiente b: "
+          b <- readLn
+          putStr "Descreva a função aqui: "
+          desc <- getLine
+          return (Funcao (Trigonometrica f a b) desc) -- a*func(bx)      
+    where
       opMin = 0
       opMax = 5
       listaOp = ["[1] Linear", "[2] Quadrática", "[3] Exponencial", "[4] Logaritimica", "[5] Trigonometrica"]
@@ -224,25 +236,112 @@ menuGeometria = do
           putStrLn "Retornando ao menu principal."
           hubMenus "principal"
 
-  where
-    listaOp = [
-        "[1] Distância entre pontos 2D",
-        "[2] Distância entre pontos 3D",
-        "[3] Ponto Médio (2D)",
-        "[4] Calcular Área",
-        "[5] Calcular Perímetro",
-        "[6] Calcular Volume",
-        "[7] Interseção entre retas",
-        "[0] Retornar"]
-    opMin = 0
-    opMax = 7
-    
+    where
+      listaOp = [
+         "[1] Distância entre pontos 2D",
+         "[2] Distância entre pontos 3D",
+         "[3] Ponto Médio (2D)",
+         "[4] Calcular Área",
+         "[5] Calcular Perímetro",
+         "[6] Calcular Volume",
+         "[7] Interseção entre retas",
+         "[0] Retornar"]
+      opMin = 0
+      opMax = 7
+      
 lerPonto2D :: IO Ponto2D
 lerPonto2D = do
-   putStr "\nEntre com a coordenada x: "
-   x <- readLn
-   putStr "Entre com a coordenada y: "
-   y <- readLn
-   return (Ponto2D x y)
+    putStr "\nEntre com a coordenada x: "
+    x <- readLn
+    putStr "Entre com a coordenada y: "
+    y <- readLn
+    return (Ponto2D x y)
+
+-- |Menu de Algoritmos
+menuAlgoritmos :: IO ()
+menuAlgoritmos = do
+    exibirOpções listaOp
+    op <- lerOpção opMin opMax
+
+    1 -> do
+        putStrLn "Realizando Merge Sort..."
+        putStr "Entre com a lista (ex: [3,1,2]): "
+        l <- readLn :: IO [Int]
+        let resultado = mergeSort l
+        putStrLn $ "Resultado: " ++ (show resultado)
+        menuAlgoritmos
+      2 -> do
+        putStrLn "Realizando Quick Sort..."
+        putStr "Entre com a lista (ex: [3,1,2]): "
+        l <- readLn :: IO [Int]
+        let resultado = quickSort l
+        putStrLn $ "Resultado: " ++ (show resultado)
+        menuAlgoritmos
+      3 -> do
+        putStrLn "Realizando Insertion Sort..."
+        putStr "Entre com a lista (ex: [3,1,2]): "
+        l <- readLn :: IO [Int]
+        let resultado = insertionSort l
+        putStrLn $ "Resultado: " ++ (show resultado)
+        menuAlgoritmos
+      4 -> do
+        putStrLn "Inserindo um elemento em uma lista ordenada..."
+        putStr "Entre com a lista ordenada (ex: [1,3,5]): "
+        l <- readLn :: IO [Int]
+        putStr "Entre com o elemento a ser inserido: "
+        v <- readLn :: IO Int
+        let resultado = inserirOrdenado v l
+        putStrLn $ "Resultado: " ++ (show resultado)
+        menuAlgoritmos
+      0 -> do
+        putStrLn "Retornando ao menu principal."
+        hubMenus "principal"
+
+    where
+      listaOp = [
+          "[1] Busca Binária",
+          "[2] Bubble Sort",
+          "[0] Retornar"
+        ]
+      opMin = 0
+      opMax = 2
+
+-- |Menu de Estruturas de Dados
+menuEstruturasDeDados :: IO ()
+menuEstruturasDeDados = do
+    exibirOpções listaOp
+    op <- lerOpção opMin opMax
+
+    case op of
       
-      
+      1 -> do
+        putStrLn "Construindo uma árvore de busca binária..."
+        putStr "Entre com a lista de elementos para a árvore (ex: [5, 3, 7, 2, 4, 6, 8]): "
+        l <- readLn :: IO [Int]
+        let arvore = construirArvore l
+        putStrLn $ "Árvore construída: " ++ (show arvore)
+        menuEstruturasDeDados
+      2 -> do
+        putStrLn "Buscando um valor na árvore..."
+        putStr "Primeiro, construa a árvore. Entre com a lista de elementos: "
+        l <- readLn :: IO [Int]
+        let arvore = construirArvore l
+        putStr "Agora, entre com o valor a ser buscado: "
+        v <- readLn :: IO Int
+        let resultado = buscarArvore v arvore
+        case resultado of
+            Just val -> putStrLn $ "Valor encontrado: " ++ show val
+            Nothing  -> putStrLn "Valor não encontrado."
+        menuEstruturasDeDados
+      0 -> do
+        putStrLn "Retornando ao menu principal."
+        hubMenus "principal"
+
+    where
+      listaOp = [
+          "[1] Construir Árvore",
+          "[2] Buscar na Árvore",
+          "[0] Retornar"
+        ]
+      opMin = 0
+      opMax = 3
