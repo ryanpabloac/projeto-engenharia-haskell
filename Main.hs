554ed349de9507f639a3dd91c9da914e9477f2cd
Caio Fernandes <caiofernandes250505@gmail.com>
2025-09-11 22:15:08 -0300

    arrumei o buffer

--- Main.hs
+++ Main.hs
@@ -1,8 +1,10 @@
 module Main (main) where
 
 import Interface (hubMenus)
+import System.IO (stdout, hSetBuffering, BufferMode(NoBuffering))
 
 main :: IO ()
-main = do
+main = do 
+    hSetBuffering stdout NoBuffering
     putStrLn "=== MENU ==="
     hubMenus "principal"
